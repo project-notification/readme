eb0480f45e36335a9a8c0d18a90ca04e052b41eb
leemhoon00 <leemhoon000@gmail.com>
2024-07-20 21:12:54 +0900

    api gateway url을 github secrets로 주입

--- subscribe.sh
+++ subscribe.sh
@@ -1,5 +1,6 @@
 # 인자로 받은 텍스트를 변수에 저장
 INPUT_TEXT="$1"
+API_URL="$2"
 
 # 입력 텍스트가 비어있는지 확인
 if [ -z "$INPUT_TEXT" ]; then
@@ -7,14 +8,11 @@ if [ -z "$INPUT_TEXT" ]; then
     exit 1
 fi
 
-echo "입력 텍스트: $INPUT_TEXT"
 # 이메일 주소 추출
 EMAIL=$(echo "$INPUT_TEXT" | grep -oP '[\w.+-]+@[\w-]+\.[\w.-]+')
 
 # 선택된 항목 추출
 TOPICS=$(echo "$INPUT_TEXT" | grep -ioP "(?<=- \[)[xX](?=\] ).*" | sed 's/^[xX]\] //' | sed 's/\r//')
-
-echo "topics: $TOPICS"
 
 # JSON 생성
 JSON_DATA=$(jq -n \
@@ -22,6 +20,7 @@ JSON_DATA=$(jq -n \
             --arg topics "$TOPICS" \
             '{email: $email, topics: ($topics | split("\n"))}')
 
-# JSON 데이터 출력 (디버깅용)
-echo "생성된 JSON 데이터:"
-echo "$JSON_DATA"
+FULL_URL="${API_URL}/subscribe"
+
+# Post 요청
+RESPONSE=$(curl -s -X POST -H "Content-Type: application/json" -d "$JSON_DATA" "$FULL_URL")
